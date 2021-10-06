01bf0b02b4f6f07a2eeb46f39b1a9c15c56f0451
Bartlomiej9867463 <73538587+Bartlomiej9867463@users.noreply.github.com>
2021-10-06 15:10:20 +0200

    Add files via upload

---
+++ po-gr1-bartlomiejaleksiejczyk/src/pl/edu/uwm/wmii/bartlomiejaleksiejczyk/laboratorium00/zad5.java
@@ -0,0 +1,9 @@
+package pl.edu.uwm.wmii.bartlomiejaleksiejczyk.laboratorium00;
+
+public class zad5 {
+    public static void main ( String [] args ) {
+        System .out. println ("+____+");
+        System .out. println ("|opel|");
+        System .out. println ("+----+");
+    }
+}
